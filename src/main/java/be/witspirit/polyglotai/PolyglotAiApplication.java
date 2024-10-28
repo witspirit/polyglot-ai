584e66d38ad4e0c1aaf5bb15a7772e8e188275b9
witspirit <witspirit@gmail.com>
2024-10-28 21:00:08 +0100

    Basic chat loop, which is super cool.

--- src/main/java/be/witspirit/polyglotai/PolyglotAiApplication.java
+++ src/main/java/be/witspirit/polyglotai/PolyglotAiApplication.java
@@ -1,10 +1,14 @@
 package be.witspirit.polyglotai;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class PolyglotAiApplication {
@@ -16,9 +20,30 @@ public class PolyglotAiApplication {
     @Bean
     public CommandLineRunner runner(ChatClient.Builder builder) {
         return args -> {
-            ChatClient chatClient = builder.build();
-            String response = chatClient.prompt("As a wizard from Lord of the Rings, tell me a joke related to a beach").call().content();
-            System.out.println(response);
+            ChatClient chatClient = builder
+                    .defaultSystem("""
+                    You are an experienced British butler, called Jarvis. You are used to catering to your
+                    boss' every need and speak in perfect Oxford English. You are always polite
+                    and tend to blend in some dry witticisms now and then.
+                    """)
+                    .defaultAdvisors(new MessageChatMemoryAdvisor(new InMemoryChatMemory()))
+                    .build();
+
+            Scanner in = new Scanner(System.in);
+
+            while (true) {
+                System.out.print("> ");
+                String input = in.nextLine();
+
+                if (input.equalsIgnoreCase("/quit")) {
+                    break;
+                }
+
+                String response = chatClient.prompt(input).call().content();
+                System.out.println(response);
+            }
+
+            System.out.println("Bye!");
         };
     }
 
