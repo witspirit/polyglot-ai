7fb150d0804c2f152185d869b68632c951d819b2
witspirit <witspirit@gmail.com>
2024-10-30 18:13:32 +0100

    Exploring Polyglot sandbox features.

--- src/main/java/be/witspirit/polyglotai/PolyglotExecutor.java
+++ src/main/java/be/witspirit/polyglotai/PolyglotExecutor.java
@@ -2,14 +2,43 @@ package be.witspirit.polyglotai;
 
 import lombok.extern.slf4j.Slf4j;
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.HostAccess;
 import org.graalvm.polyglot.PolyglotException;
+import org.graalvm.polyglot.SandboxPolicy;
 import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 
 @Service
 @Slf4j
 public class PolyglotExecutor {
 
-    private static final Context context = Context.create();
+    private final OutputStream programOutput;
+    private final OutputStream programError;
+
+    private final Context jsContext;
+    private final Context pyContext;
+
+    public PolyglotExecutor() {
+        programOutput = new ByteArrayOutputStream();
+        programError = new ByteArrayOutputStream();
+
+        jsContext = Context.newBuilder("js")
+                .sandbox(SandboxPolicy.CONSTRAINED)
+                .out(programOutput)
+                .err(programError)
+                // .option("sandbox.MaxStatements", "2") // Requires enterprise runtime
+                .build();
+
+        pyContext = Context.newBuilder("python")
+                .sandbox(SandboxPolicy.TRUSTED) // Constrained is not supported for Python
+                .allowHostAccess(HostAccess.CONSTRAINED)
+                .out(programOutput)
+                .err(programError)
+                // .option("sandbox.MaxStatements", "2") // Requires Enterprise runtime
+                .build();
+    }
 
     public boolean isJavascript(String codeCandidate) {
         try {
@@ -17,7 +46,7 @@ public class PolyglotExecutor {
             if (codeCandidate == null || codeCandidate.isEmpty()) {
                 return false;
             }
-            context.eval("js", codeCandidate);
+            jsContext.eval("js", codeCandidate);
             return true;
         } catch (PolyglotException e) {
             log.info("Failed to execute JS: {}", e.getMessage());
@@ -31,7 +60,7 @@ public class PolyglotExecutor {
             if (codeCandidate == null || codeCandidate.isEmpty()) {
                 return false;
             }
-            context.eval("python", codeCandidate);
+            pyContext.eval("python", codeCandidate);
             return true;
         } catch (PolyglotException e) {
             log.info("Failed to execute Python: {}", e.getMessage());
